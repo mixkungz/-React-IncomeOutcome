66f68d713cdb3a862e742745442cf775de9f40d3
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2017-03-10 03:00:18 +0700

    change alert to sweetalert2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import { default as swal } from 'sweetalert2/dist/sweetalert2.min.js';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 ReactDOM.render(
   <App />,
