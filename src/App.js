6bafb1be897976e915b5154fd7e2edd129e24b5a
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2017-03-09 23:58:26 +0700

    send data from form to func + add function calculate

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
 import React from 'react';
-import 
 
 class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      "money":0,
+      "type":"income",
+      data :[]
+    }
+  }
+  updateState(){
+    this.setState({
+      money:document.getElementById('inputmoney').value,
+      type:document.getElementById('selecttype').value,
+    });
+  }
+
+  submit(e){
+    e.preventDefault();
+    this.state.data.push({
+      money:this.state.money,
+      type:this.state.type
+    })
+    this.setState({
+      money:0,
+      type:"income"
+    })
+  }
+  calculate(){
+    let x =0;
+    for(let i=0;i<this.state.data.length;i++){
+      if(this.state.data[i].type ==='income'){
+        x+=parseInt(this.state.data[i].money)
+      }
+      else{
+        x-=parseInt(this.state.data[i].money)
+      }
+    }
+    return x
+  }
   render(){
     return(
         <div>
-          <form>
-            <input type="text"/>
-            <ButtonSubmit />
-            <ButtonReset />
+          <form onSubmit={this.submit.bind(this)}>
+            <input id="inputmoney" type="number" name="money" value={this.state.money} onChange={this.updateState.bind(this)}/>
+            <select id="selecttype" name="type" onChange={this.updateState.bind(this)}>
+              <option value="income" selected>รายรับ</option>
+              <option value="outcome">รายจ่าย</option>
+            </select>
+            <br />
+            <button type="submit" >Submit</button>
+            <input type="reset" value="Reset"/>
           </form>
+          <p>{this.calculate()}</p>
+          
         </div>
       );
   }
 }
-class ButtonSubmit extends React.Component{
-  render(){
-    return <input type="submit" value="Submit"/>;
-  }
-}
-class ButtonReset extends React.Component{
-  render(){
-    return <input type="reset" value="Reset"/>;
-  }
-}
+
+
 export default App
