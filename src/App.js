de8c89697ce5e323e4b5c6d91bb20a45dc20968d
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2017-03-10 02:47:16 +0700

    add genIncome & genOutcome function + some bootstrap

--- src/App.js
+++ src/App.js
@@ -18,14 +18,19 @@ class App extends React.Component{
 
   submit(e){
     e.preventDefault();
-    this.state.data.push({
-      money:this.state.money,
-      type:this.state.type
-    })
-    this.setState({
-      money:0,
-      type:"income"
-    })
+    if(this.state.money <= 0){
+      alert('Money must more than 0')
+    }
+    else{
+      this.state.data.push({
+        money:this.state.money,
+        type:this.state.type
+      })
+      this.setState({
+        money:0,
+        type:"income"
+      })
+    }
   }
   calculate(){
     let x =0;
@@ -39,21 +44,54 @@ class App extends React.Component{
     }
     return x
   }
+  genIncomeLog(){
+    let x ="";
+    let runnum=1;
+    for (let i = 0; i < this.state.data.length; i++) {
+      if(this.state.data[i].type ==='income'){
+        x+=runnum+"). "+this.state.data[i].money+'<br/>'
+        runnum++
+      }
+    }
+    return x
+  }
+  genOutcomeLog(){
+    let x ="";
+    let runnum=1;
+    for (let i = 0; i < this.state.data.length; i++) {
+      if(this.state.data[i].type ==='outcome'){
+        x+=runnum+"). "+this.state.data[i].money+'<br/>'
+        runnum++
+      }
+    }
+    return x
+  }
   render(){
     return(
-        <div>
+        <div className="container" style={{textAlign:'center'}}>
           <form onSubmit={this.submit.bind(this)}>
             <input id="inputmoney" type="number" name="money" value={this.state.money} onChange={this.updateState.bind(this)}/>
-            <select id="selecttype" name="type" onChange={this.updateState.bind(this)}>
+            <select id="selecttype" name="type" onChange={this.updateState.bind(this)} style={{marginLeft:'1em'}}>
               <option value="income" selected>รายรับ</option>
               <option value="outcome">รายจ่าย</option>
             </select>
             <br />
-            <button type="submit" >Submit</button>
-            <input type="reset" value="Reset"/>
+            <div style={{marginTop:'1em'}}>
+              <button type="submit" className="btn btn-success">Submit</button>
+              <input type="reset" className="btn btn-danger" value="Reset" style={{marginLeft:'1em'}}/>
+            </div>
           </form>
-          <p>{this.calculate()}</p>
-          
+          <div className="row">
+            <div className="col-xs-6">
+              <p>รายรับ</p>
+              <div dangerouslySetInnerHTML={{__html: this.genIncomeLog()}} />
+            </div>
+            <div className="col-xs-6">
+              <p>รายจ่าย</p>
+              <div dangerouslySetInnerHTML={{__html: this.genOutcomeLog()}} />
+            </div>
+          </div>
+          <p>เหลือ : {this.calculate()}</p>
         </div>
       );
   }
